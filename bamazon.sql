e955c99a7478c0e60f3d58e280bbe99bf44620ac
msa0683 <msa0683@yahoo.com>
2017-07-20 16:51:12 -0700

    added collumns to product table

--- bamazon.sql
+++ bamazon.sql
@@ -1,6 +1,13 @@
-CREATE DATABASE bamazon_db
+DROP DATABASE IF EXISTS bamazon_db;
+CREATE DATABASE bamazon_db;
 
 USE bamazon_db;
 
 CREATE TABLE products (
+	id INTEGER(10) AUTO_INCREMENT NOT NULL,
+	product_name VARCHAR(50) NOT NULL,
+	department_name VARCHAR(50) NOT NULL,
+	price INTEGER(50) NOT NULL,
+	stock_quantity INTEGER(50) NOT NULL
+	PRIMARY KEY (id)
 );
