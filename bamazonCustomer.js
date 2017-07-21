46c49c5eeff17f38de60fc6788f8e7143ce779d3
msa0683 <msa0683@yahoo.com>
2017-07-20 18:29:24 -0700

    mysql connection code completed

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,19 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+
+	host: "localhost", 
+	port: 3306,
+
+	user: "root",
+	password: "mariam",
+	database: "bamazon_db"
+});
+
+connection.connect(function(err) {
+	if (err) throw err;
+	else {
+		console.log("connected!");
+	}
+});
