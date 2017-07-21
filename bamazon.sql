13d95ce02522714353f1d10421e050f92af1b66c
msa0683 <msa0683@yahoo.com>
2017-07-20 17:38:16 -0700

    added columns and 10 products to product table

--- bamazon.sql
+++ bamazon.sql
@@ -11,3 +11,38 @@ CREATE TABLE products (
 	stock_quantity INTEGER(50) NOT NULL
 	PRIMARY KEY (id)
 );
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Peg Perego High Chair", "High chairs and walkers", 300, 100);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Joovy Spoon Walker", "High chairs and walkers", 100, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Baby Jogger City Select Double Stroller", "Car Seats & Strollers", 500, 70);
+
+INSERT INSERT products (product_name, department_name, price, stock_quantity)
+VALUES ("Peg Perego Primo Viaggio Convertible Car Seat - Licorice", "Car Seats & Strollers", 400, 100);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Mommy Hook", "Baby Essentials", 8, 100);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Fawn Design Baby Bag", "Baby Essentials", 120, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Baby K'tan  Baby Carrier", "Baby Essentials", 50, 100);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("BreathableBaby Breathable Mesh Crib Liner", "Nursery", 25, 100);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("4moms breeze Playard", "Nursery", 300, 50);
+
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Aden and Anais Swaddle Blankets", "Nursery", 30, 100);
+
+
+
+
